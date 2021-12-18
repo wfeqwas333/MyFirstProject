0c3dfe4eecfc624d1f8dd9c369d6f0cc6f27e7a4
wfeqwas333 <92229298+wfeqwas333@users.noreply.github.com>
2021-12-17 22:55:43 -0600

    Add files via upload

---
+++ code/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+####################################################
+
+#Твой код
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('index.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/teach')
+def teach():
+	return render_template('teach.html')
+
+####################################################
+
+if __name__ == "__main__":
+  app.run(debug=True)
